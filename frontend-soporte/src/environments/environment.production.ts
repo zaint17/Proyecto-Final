e9bcb7a9c3002e6eb2573360ef043a0c98b4c011
unknown <zaintevilord@gmail.com>
2026-06-19 23:23:41 -0500

    Frontend apuntando al backend real de Render

--- frontend-soporte/src/environments/environment.production.ts
+++ frontend-soporte/src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://TU-BACKEND-EN-PRODUCCION/api'
+  apiUrl: 'https://soporte-backend-jaq7.onrender.com/api' // O como tengas estructurada tu ruta base
 };
