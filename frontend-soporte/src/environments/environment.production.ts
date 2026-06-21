89592f24d54c9317d857c7e871915811cccc3cb6
unknown <zaintevilord@gmail.com>
2026-06-21 00:35:57 -0500

    Fix: corregir /api en las urls de los environments

--- frontend-soporte/src/environments/environment.production.ts
+++ frontend-soporte/src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://soporte-backend-jaq7.onrender.com/api' // O como tengas estructurada tu ruta base
+  apiUrl: 'https://soporte-backend-jaq7.onrender.com/api'
 };
--- frontend-soporte/src/environments/environment.ts
+++ frontend-soporte/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  // Por tu URL real de Render:
   apiUrl: 'https://soporte-backend-jaq7.onrender.com/api'
 };
